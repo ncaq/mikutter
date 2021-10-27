660101968b0b13b20bba502d911c67c6cd3e558c
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2021-10-27 23:47:58 +0900

    特定のタイムラインで BT/RT表示が生RT表示になっている問題の修正案

--- plugin/gtk3/widget/timeline.rb
+++ plugin/gtk3/widget/timeline.rb
@@ -96,7 +96,8 @@ module Plugin::Gtk3
 
     def bulk_add(models)
       update_ordinal = false
-      models.each do |message|
+      models.each do |m|
+        message = m.retweet_source || m
         mp = find_miracle_painter_by_message(message)
         if mp
           update_ordinal |= @order.(mp.model) != @order.(message)
